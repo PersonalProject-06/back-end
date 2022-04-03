dc2fe472a479001d864370010d136d2e9ecd0e49
firas-debich <debichf02@gmail.com>
2022-04-04 00:51:35 +0100

    single chat component

--- front/src/components/miscellaneous/ChatBox.jsx
+++ front/src/components/miscellaneous/ChatBox.jsx
@@ -1,12 +1,22 @@
-import React ,{useContext} from 'react'
-import {ChatContext} from '../../context/ChatProvider'
+import React, { useContext } from "react";
+import { ChatContext } from "../../context/ChatProvider";
+import { Box , useColorMode, } from '@chakra-ui/react';
 
 const ChatBox = () => {
-  const {slectedChat} = useContext(ChatContext)
-  console.log(user);
-  return (
-    <div>ChatBox</div>
-  )
-}
+  const { colorMode } = useColorMode();
+  const { slectedChat } = useContext(ChatContext);
 
-export default ChatBox
+  return <Box
+    d={{ base: slectedChat ? "flex" : "none", md: "flex" }}
+    flexDir={"column"}
+    alignItems="center"
+    p={3}
+   bg={colorMode === "light" ? "white" : "gray.800"}
+    w={{ base: "100%", md: "68%" }}
+    borderRadius="lg"
+    borderWidth={"1px"}
+
+  >ChatBox</Box>;
+};
+
+export default ChatBox;
